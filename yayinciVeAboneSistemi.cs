0f2a957fcbd0e9c6e5a62401aef59fe8878ceeca
muharremtavli <muharremtavli2@gmail.com>
2024-12-16 17:42:23 +0300

    Add files via upload

---
+++ yayinciVeAboneSistemi.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+
+// IYayinci Arayüzü
+public interface IYayinci
+{
+    void AboneEkle(IAbone abone);
+    void AboneCikar(IAbone abone);
+    void AboneListele();
+    void Bildir();
+}
+
+// IAbone Arayüzü
+public interface IAbone
+{
+    void BilgiAl(string mesaj);
+}
+
+// Yayinci Sınıfı
+public class Yayinci : IYayinci
+{
+    private List<IAbone> aboneler = new List<IAbone>();
+    private string haber;
+
+    public void AboneEkle(IAbone abone)
+    {
+        aboneler.Add(abone);
+        Console.WriteLine("Abone eklendi.");
+    }
+
+    public void AboneCikar(IAbone abone)
+    {
+        aboneler.Remove(abone);
+        Console.WriteLine("Abone çıkarıldı.");
+    }
+
+    public void AboneListele()
+    {
+        Console.WriteLine($"Toplam Abone Sayısı: {aboneler.Count}");
+    }
+
+    public void Bildir()
+    {
+        foreach (var abone in aboneler)
+        {
+            abone.BilgiAl(haber);
+        }
+    }
+
+    public string Haber
+    {
+        get { return haber; }
+        set
+        {
+            haber = value;
+            Console.WriteLine($"Yeni haber: {haber}");
+            Bildir();
+        }
+    }
+}
+
+// Abone Sınıfı
+public class Abone : IAbone
+{
+    private string isim;
+
+    public Abone(string isim)
+    {
+        this.isim = isim;
+    }
+
+    public void BilgiAl(string mesaj)
+    {
+        Console.WriteLine($"{isim} abonesi, yeni haber aldı: {mesaj}");
+    }
+}
+
+// Ana Program
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Yayıncı oluştur
+        Yayinci yayinci = new Yayinci();
+
+        // Aboneler oluştur
+        Abone abone1 = new Abone("Ali");
+        Abone abone2 = new Abone("Ayşe");
+
+        // Aboneleri yayıncıya ekle
+        yayinci.AboneEkle(abone1);
+        yayinci.AboneEkle(abone2);
+
+        // Aboneleri listele
+        yayinci.AboneListele();
+
+        // Yayıncı bir haber gönderdiğinde
+        yayinci.Haber = "Yeni kitap çıktı!";
+
+        // Bir abone çıkardığında
+        yayinci.AboneCikar(abone1);
+
+        // Başka bir haber gönderdiğinde
+        yayinci.Haber = "Yaz indirimleri başladı!";
+    }
+}
---
+++ yazilimveMuhasebeSinifi.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+class Calisan
+{
+    public string Ad { get; set; }
+    public string Soyad { get; set; }
+    public double Maas { get; set; }
+    public string Pozisyon { get; set; }
+
+    public Calisan(string ad, string soyad, double maas, string pozisyon)
+    {
+        Ad = ad;
+        Soyad = soyad;
+        Maas = maas;
+        Pozisyon = pozisyon;
+    }
+
+    public virtual string BilgiYazdir()
+    {
+        return $"Çalışan: {Ad} {Soyad}, Pozisyon: {Pozisyon}, Maaş: {Maas} TL";
+    }
+}
+
+class Yazilimci : Calisan
+{
+    public string YazilimDili { get; set; }
+
+    public Yazilimci(string ad, string soyad, double maas, string yazilimDili)
+        : base(ad, soyad, maas, "Yazılımcı")
+    {
+        YazilimDili = yazilimDili;
+    }
+
+    public override string BilgiYazdir()
+    {
+        return base.BilgiYazdir() + $", Yazılım Dili: {YazilimDili}";
+    }
+}
+
+class Muhasebeci : Calisan
+{
+    public string MuhasebeYazilimi { get; set; }
+
+    public Muhasebeci(string ad, string soyad, double maas, string muhasebeYazilimi)
+        : base(ad, soyad, maas, "Muhasebeci")
+    {
+        MuhasebeYazilimi = muhasebeYazilimi;
+    }
+
+    public override string BilgiYazdir()
+    {
+        return base.BilgiYazdir() + $", Kullandığı Muhasebe Yazılımı: {MuhasebeYazilimi}";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Çalışan türünü seçin: ");
+        Console.WriteLine("1. Yazılımcı");
+        Console.WriteLine("2. Muhasebeci");
+
+        string secim = Console.ReadLine();
+        Calisan calisan = null;
+
+        if (secim == "1")
+        {
+            Console.Write("Ad: ");
+            string ad = Console.ReadLine();
+            Console.Write("Soyad: ");
+            string soyad = Console.ReadLine();
+            Console.Write("Maaş: ");
+            double maas = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Yazılım Dili: ");
+            string yazilimDili = Console.ReadLine();
+            calisan = new Yazilimci(ad, soyad, maas, yazilimDili);
+        }
+        else if (secim == "2")
+        {
+            Console.Write("Ad: ");
+            string ad = Console.ReadLine();
+            Console.Write("Soyad: ");
+            string soyad = Console.ReadLine();
+            Console.Write("Maaş: ");
+            double maas = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Muhasebe Yazılımı: ");
+            string muhasebeYazilimi = Console.ReadLine();
+            calisan = new Muhasebeci(ad, soyad, maas, muhasebeYazilimi);
+        }
+        else
+        {
+            Console.WriteLine("Geçersiz seçim!");
+            return;
+        }
+
+        Console.WriteLine(calisan.BilgiYazdir());
+        Console.ReadLine();
+    }
+}
